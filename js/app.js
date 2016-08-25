693957dd9497ef2d14cb746cd440da196679e58d
cmpereirasi <cmpereira50@gmail.com>
2016-08-25 19:46:15 -0300

    importando google fonte e font-awesome

--- js/app.js
+++ js/app.js
@@ -1,4 +1,1 @@
-  document.getElementById("logo").innerHTML = "Gulp ";
-  document.getElementById("descricao").innerHTML = "lorem img-thumbnail";
-  document.getElementById("logo").innerHTML = "Gulp ";
-  document.getElementById("logo").innerHTML = "Gulp ";
+
